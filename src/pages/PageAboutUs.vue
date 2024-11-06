9416a17259b0b49ec1725aaef57c21c49468c384
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-11-06 16:38:28 +0100

    [UPD] updated PageAboutUs.vue

--- src/pages/PageAboutUs.vue
+++ src/pages/PageAboutUs.vue
@@ -5,9 +5,46 @@ export default {
 </script>
 
 <template>
-  <div>
-    <h1>Su di noi</h1>
-    <p>Questa è la pagina "Su di noi", dove puoi saperne di più su di noi e sul nostro lavoro.</p>
+  <div class="container">
+    <div class="row">
+      <section id="about-us">
+        <h2>Chi Siamo</h2>
+        <p>
+          BDoctors è un'azienda medica che si dedica a migliorare l'accesso alle cure sanitarie per tutti. La nostra piattaforma permette ai pazienti di trovare rapidamente il medico di cui hanno bisogno e di prenotare una visita con facilità.
+        </p>
+        <p>
+          Siamo composti da un team di professionisti che lavora per rendere l'assistenza sanitaria più accessibile e meno complessa. Crediamo che ognuno debba avere la possibilità di prendersi cura della propria salute in modo comodo e sicuro.
+        </p>
+      </section>
+    </div>
+    <div class="row">
+      <section id="mission-vision">
+        <h3>La nostra Mission</h3>
+        <p>
+          La mission di BDoctors è offrire a ogni paziente l'accesso a un'assistenza sanitaria di qualità, attraverso una piattaforma innovativa e facile da usare. Il nostro obiettivo è semplificare il processo di ricerca e prenotazione delle visite mediche, rendendo tutto a portata di clic.
+        </p>
+    
+        <h3>La nostra Vision</h3>
+        <p>
+          Immaginiamo un mondo dove ogni individuo possa prendersi cura della propria salute senza difficoltà. BDoctors lavora per abbattere le barriere tra pazienti e medici, creando un sistema di assistenza sanitaria più accessibile per tutti.
+        </p>
+      </section>
+    </div>
+    <div class="row">
+      <section id="team">
+        <h2>Il Nostro Team</h2>
+        <p>
+          Il nostro team è composto da medici, esperti di tecnologia e assistenza clienti, tutti impegnati per offrire il meglio ai nostri pazienti. Con competenza e dedizione, siamo pronti ad assistervi in ogni fase del vostro percorso di cura.
+        </p>
+      </section>
+    </div>
+    <div class="row">
+      <section id="search-and-book">
+        <h2>Cerca un Medico</h2>
+        <p>Su BDoctors, puoi cercare il medico giusto per te in base alla specializzazione, alla posizione e alla disponibilità. La nostra piattaforma ti permette di prenotare facilmente una visita, con pochi clic.</p>
+        <a href="doctors.html" class="cta-button">Trova un Medico</a>
+    </section>
+    </div>
   </div>
 </template>
 
