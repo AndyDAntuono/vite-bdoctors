4b7d5a8702726aa947b28f705601f3dc0f667be9
pierobramanti <piero.bramanti94@gmail.com>
2024-11-08 17:05:34 +0100

    [UPD] Axios call in SingleDoctorPage working successfully

--- src/pages/PageSingleDoctor.vue
+++ src/pages/PageSingleDoctor.vue
@@ -1,38 +1,55 @@
 <script>
-import { store } from '../store';
+import { store } from '../store.js'; 
 import axios from 'axios';
+
 export default {
-    // DATA inizializzare doctor a '' e usare lo store
-    // METHODS getter dei dettagli del doctor con axios all'endopoint url dei dottori + il parametro dello slug:
-    // this.$route.params.slug
-    // CREATED richiamare metodo
+    data(){
+        return {
+            store,
+            doctor: null, 
+        }
+    },
+    created() {
+        this.getsingleDoctor(); 
+    },
+    methods: {
+        getsingleDoctor() {
+            axios.get(`${store.url}/doctors/${this.$route.params.slug}`).then(res => {  
+                if(res.data.success) {
+                    this.doctor = res.data.results; 
+                }
+            });
+        }
+    },
+   
 }
 </script>
+
 <template>
-    <template>
-        <div class="container my-5">
-            <div class="row">
-                <div class="col-12 col-md-4 text-center">
-                    <img :src="doctor.thumb" alt="doctor-profile-image" class="img-fluid rounded-circle mb-3">
-                    <h2>{{ doctor.user_name }} {{ doctor.user_surname }}</h2>
-                    <p class="text-muted">{{ doctor.address }}, {{ doctor.city }}</p>
-                </div>
-                <div class="col-12 col-md-8">
-                    <h3>Specializzazioni</h3>
-                    <ul class="list-unstyled">
-                        <li v-for="field in doctor.fields" :key="field.id">{{ field.name }}</li>
-                    </ul>
-                    
-                    <h3>Contatti</h3>
-                    <p>Telefono: {{ doctor.phone_number }}</p>
-    
-                    <h3>Descrizione</h3>
-                    <p>{{ doctor.performance }}</p>
-                </div>
-            </div>
-        </div>
-    </template>
-</template>
+    <div class="container my-5">
+         <div class="row">
+             <div class="col-12 col-md-4 text-center">
+                <img class="rounded-circle img-fluid" :src="doctor.thumb && doctor.thumb.startsWith('http') ? doctor.thumb : (doctor.thumb ? `http://127.0.0.1:8000/storage/${doctor.thumb}` : '')" :alt="`${doctor.user_surname}-image`">
+                 <h2>{{ doctor.user_name }} {{ doctor.user_surname }}</h2>
+                 <p class="text-muted">{{ doctor.address }}, {{ doctor.city }}</p>
+             </div>
+             <div class="col-12 col-md-8">
+                 <h3>Specializzazioni</h3>
+                 <ul class="list-unstyled">
+                    <li v-for="field in doctor.fields" :key="field.id">{{ field.name }}</li>
+                </ul>
+                 
+                 <h3>Contatti</h3>
+                 <p>Telefono: {{ doctor.phone_number }}</p>
+ 
+                 <h3>Descrizione</h3>
+                 <p>{{ doctor.performance }}</p>
+             </div>
+         </div>
+    </div>
+ </template>
+ 
+
 <style lang="scss" scoped>
     
 </style>
