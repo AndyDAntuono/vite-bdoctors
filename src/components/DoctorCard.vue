1f5f9b0fcbb22f89b0d508880148552aa81099c4
pierobramanti <piero.bramanti94@gmail.com>
2024-11-08 12:31:45 +0100

    [UPD] working on cards style

--- src/components/DoctorCard.vue
+++ src/components/DoctorCard.vue
@@ -12,8 +12,8 @@ export default {
 </script>
 <template>
     <div class="col-12 col-md-6 col-lg-4 mt-5">
-        <div class="card min-h">
-            <div class="box-avatars mx-auto">
+        <div class="card h-100">
+            <div class="img-container mx-auto">
                 <img class="card-img-top img-fluid" :src="doctor.thumb.startsWith('http') ? doctor.thumb : `http://127.0.0.1:8000/storage/${doctor.thumb}`" :alt="`${doctor.user_surname}-image`">
             </div>
             <div class="card-body">
@@ -22,7 +22,7 @@ export default {
                     <li class="card-text me-2 fw-bold list" v-for="field in doctor.fields">{{field.name}}</li>
                 </ul>
                 <span>{{doctor.address}} ({{ doctor.city.charAt(0).toUpperCase() + doctor.city.slice(1).toLowerCase() }})</span>
-                <p class="card-text">{{trText(doctor.performance)}}</p>
+                <p class="card-text fw-bold py-2">{{trText(doctor.performance)}}</p>
             </div>
         </div>
     </div>
@@ -30,21 +30,37 @@ export default {
 <style lang="scss" scoped>
 @import '../styles/generals.scss';
 
-.min-h {
-    min-height: 500px;
+.card {
+    min-height: 150px;
     background-color: $navy-blue;
     color: $pure-white;
     border: none;
     box-shadow: 3px 3px 5px $dark-grey;
-    .box-avatars {
-        width: 300px;
-        height: 300px;
-        img {
-            max-width: 100%;
-            max-height: 100%;
-            object-fit: contain;
-        }
+    transition: transform 0.5s ease, box-shadow 0.5s ease; 
+
+    &:hover {
+        transform: translateY(5px); 
+        box-shadow: 5px 5px 10px $dark-grey; 
+        cursor: pointer;
     }
+
+    .img-container {
+        padding: 5px; 
+        background-color: $navy-blue; 
+        border-radius: 10px; 
+        max-width: 80%; 
+    }
+
+    .card-img-top {
+        border-radius: 5px; 
+        width: 100%;
+    }
+
+    .card-body {
+        background-color: $navy-blue;
+        border-radius: 10px; 
+    }
+
     .card-title {
         font-size: 24px;
         letter-spacing: 1px;
@@ -53,6 +69,11 @@ export default {
     .list {
         color: $aqua-green;
     }
+
+    .card-text{
+        font-size: 15px;
+    }
 }
+
 
 </style>
