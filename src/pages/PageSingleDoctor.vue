e7a36c65e20b74aaa05257e65cfa14a4e7298724
Corfaya <lauracorrado0696@gmail.com>
2024-11-08 12:00:27 +0100

    [UPD] PageSingleDoctor scaffolding with comments

--- src/pages/PageSingleDoctor.vue
+++ src/pages/PageSingleDoctor.vue
@@ -1,6 +1,11 @@
 <script>
+import { store } from '../store';
+import axios from 'axios';
 export default {
-    
+    // DATA inizializzare doctor a '' e usare lo store
+    // METHODS getter dei dettagli del doctor con axios all'endopoint url dei dottori + il parametro dello slug:
+    // this.$route.params.slug
+    // CREATED richiamare metodo
 }
 </script>
 <template>
