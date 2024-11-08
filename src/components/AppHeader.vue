ca6730bba0d158035c985513db5f8c28a0cfe612
Corfaya <lauracorrado0696@gmail.com>
2024-11-08 15:34:18 +0100

    [UPD] offcanvas style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -53,12 +53,12 @@ export default {
               <ul class="dropdown-menu" aria-labelledby="doctorDropdown">
                 <li>
                   <a class="dropdown-item" href="http://127.0.0.1:8000/register" target="_blank">
-                    Registrati
+                    <i class="bi bi-pen-fill"></i> Registrati
                   </a>
                 </li>
                 <li>
                   <a class="dropdown-item" href="http://127.0.0.1:8000/" target="_blank">
-                    Area professionisti
+                    <i class="bi bi-clipboard2-pulse-fill"></i> Area professionisti
                   </a>
                 </li>
               </ul>
@@ -67,29 +67,32 @@ export default {
 
           <!-- Offcanvas (mobile) -->
           <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasMenu" aria-labelledby="offcanvasMenuLabel">
-            <div class="offcanvas-header">
-              <h5 class="offcanvas-title" id="offcanvasMenuLabel">BDoctors</h5>
+            <div class="offcanvas-header py-4">
+              <h5 class="offcanvas-title fw-bolder" id="offcanvasMenuLabel">BDoctors</h5>
+              <span class="doc-line d-flex ms-2">
+                <img src="../../public/pngegg.png" alt="">
+              </span>
               <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div class="offcanvas-body">
               <ul class="nav-list d-flex flex-column align-items-start mb-0">
                 <li v-for="(item, index) in menu" :key="index">
-                  <router-link class="text-decoration-none links" :to="{ name: item.name }">{{ item.label
+                  <router-link class="text-decoration-none links-canvas" :to="{ name: item.name }">{{ item.label
                     }}</router-link>
                 </li>
 
                 <!-- Dropdown (mobile) -->
-                <li class="nav-item dropdown w-100">
-                  <a class="nav-link dropdown-toggle fw-bold custom-dropdown rounded" href="#" id="doctorDropdownMobile"
+                <li class="nav-item dropdown w-100 mt-2">
+                  <a class="nav-link dropdown-toggle fw-bold custom-dropdown-canvas rounded" href="#" id="doctorDropdownMobile"
                     role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Sei un medico?
                   </a>
                   <ul class="dropdown-menu" aria-labelledby="doctorDropdownMobile">
                     <li>
-                      <a class="dropdown-item" href="http://127.0.0.1:8000/register" target="_blank">Registrati</a>
+                      <a class="dropdown-item" href="http://127.0.0.1:8000/register" target="_blank"><i class="bi bi-pen-fill"></i> Registrati</a>
                     </li>
                     <li>
-                      <a class="dropdown-item" href="http://127.0.0.1:8000/" target="_blank">Area professionisti</a>
+                      <a class="dropdown-item" href="http://127.0.0.1:8000/" target="_blank"><i class="bi bi-clipboard2-pulse-fill"></i> Area professionisti</a>
                     </li>
                   </ul>
                 </li>
@@ -132,6 +135,16 @@ export default {
   }
 }
 
+.links-canvas {
+  color: $navy-blue;
+  padding: 10px 0px;
+  margin-bottom: 15px;
+
+  &:hover {
+    color: $aqua-green;
+  }
+}
+
 .logo {
   width: 70px;
   height: auto;
@@ -149,10 +162,59 @@ export default {
   }
 }
 
+.custom-dropdown-canvas {
+  color: $navy-blue;
+  padding: 10px 0px;
+  transition: all 0.5s;
+
+  &:hover {
+    color: #000;
+  }
+}
+
 .bi-list-nested, .bi-list {
   color: $pure-white;
   font-size: 30px;
   transition: all 2s ease;
-  }
+}
 
+.offcanvas {
+  .dropdown-menu{
+    border: none;
+  }
+}
+
+.offcanvas-body {
+  padding: 10px;
+}
+
+.offcanvas-header {
+  padding: 10px 20px;
+  padding-left: 20px;
+  background-color: $navy-blue;
+  .offcanvas-title {
+    letter-spacing: 1px;
+    color: $pure-white;
+  }
+  .doc-line {
+    width: 40px;
+    height: 20px;
+    vertical-align: middle;
+    img {
+      max-width: 100%;
+      max-height: 100%;
+      object-fit: contain;
+      filter: brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(7500%) hue-rotate(137deg) brightness(107%) contrast(102%);
+    }
+  }
+}
+
+.offcanvas-body {
+  padding: 10px 20px;
+  padding-top: 30px;
+}
+
+.dropdown-menu {
+  color: $navy-blue;
+}
 </style>
