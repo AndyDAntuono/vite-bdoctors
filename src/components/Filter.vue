a4317ef9a55f4ffb02d37b436ba8c4a92aa22d39
pierobramanti <piero.bramanti94@gmail.com>
2024-11-10 17:41:00 +0100

    [UPD] Created basic form layout in PageAdvanceResearch

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -66,7 +66,7 @@ export default {
 
     <!-- Radio Button per selezionare il criterio di ordinamento -->
     <div class="sort-criteria">
-      <h5 class="w-25 text-center p-1 rounded"><router-link class="text-decoration-none"to="/advanced-research">Ricerca Avanzata</router-link>
+      <h5 class="w-25 text-center p-1 rounded"><router-link class="text-decoration-none text-white"to="/advanced-research">Ricerca Avanzata</router-link>
       </h5>
       <label><strong>Ordina per:</strong></label>
       <div class="radio-options">
