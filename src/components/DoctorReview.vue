8a12434064e331b63ed4234584a6241558b28228
pierobramanti <piero.bramanti94@gmail.com>
2024-11-11 18:37:01 +0100

    [UPD] stil working on style DoctorReview

--- src/components/DoctorReview.vue
+++ src/components/DoctorReview.vue
@@ -4,7 +4,7 @@ export default {
     props: {
         review: {
             type: Object,
-            
+           
         }
     }
 };
@@ -17,40 +17,57 @@ export default {
             <h5 class="review-name">{{ review.name }}</h5>
         </div>
         <div class="d-flex align-items-center text-muted">
-            <i class="bi bi-envelope-fill me-2"></i>
+            <i class="bi bi-envelope-fill me-2 text-primary"></i>
             <p class="review-email mb-0">{{ review.email }}</p>
         </div>
-        <p class="review-content mt-3">{{ review.content }}</p>
+        <hr class="my-3 custom-hr" />
+        <p class="review-content mt-1">{{ review.content }}</p>
     </div>
 </template>
 
 <style lang="scss" scoped>
 @import '../styles/generals.scss';
 
+// Nuovi colori
+$card-bg-color: #f5f7fa; 
+$primary-text-color: #333333; 
+$secondary-text-color: #4a90e2; 
+$separator-color: #d1d8e0; 
 
 .review-card {
-    border: 1px solid $light-gray;
+    border: 1.5px solid $separator-color;
     padding: 1rem;
-    border-radius: 8px;
-    background-color: $light-blue;
+    border-radius: 5px;
+    background-color: $card-bg-color;
 
     .review-name {
-        font-size: 1.5rem;
-        font-weight: bold;
-        color: $navy-blue;
+        font-size: 1.1rem;
+        font-weight: lighter;
+        color: $secondary-text-color;
+        text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1);
     }
 
     .review-email {
-        font-size: 1 rem;
-        color: $dark-grey;
+        font-size: 0.8rem;
+        color: $secondary-text-color;
+        font-weight: lighter;
+        text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1);
     }
 
     .review-content {
         font-size: 1rem;
-        color: $dark-grey;
         line-height: 1.5;
+        color: $primary-text-color;
+        font-weight: normal;
+        text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.2);
     }
+}
 
-
+.custom-hr {
+    height: 3px;
+    width: 100%; 
+    background-color: $separator-color;
+    border: none;
+    margin: 1.5rem auto;
 }
 </style>
